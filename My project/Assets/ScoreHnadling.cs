b20a214bd43fb0af343506f73614588cf2ed292b
Zacharie Lacombe <zacharielacombe@pop-os.localdomain>
2023-02-03 10:33:26 +0100

    feat: add scoring system with setter

---
+++ My project/Assets/ScoreHnadling.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreHnadling : MonoBehaviour
+{
+    int score;
+    public Text scoreText;
+
+    void scoreIncrease(int addValue)
+    {
+        score += addValue;
+        scoreText.text = "Score\n".ToString() + score.ToString();
+    }
+
+    void scoreDecrease(int subValue)
+    {
+        score -= subValue;
+        scoreText.text = "Score\n".ToString() + score.ToString();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+        scoreText.text = "Score\n".ToString() + score.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
